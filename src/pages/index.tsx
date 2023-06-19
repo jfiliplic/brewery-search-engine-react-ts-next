28dfbefb7997ad053d6cd4b606d77708474de37b
jfiliplic <97060179+jfiliplic@users.noreply.github.com>
2023-06-19 19:40:51 +0200

    wrapped functions in useCallback, fixed bug in onSearchbarSubmit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,80 +10,94 @@ export default function Home() {
   const [keyword, setKeyword] = useState("name");
   const [text, setText] = useState("");
   const [submitted, setSubmitted] = useState("");
-  const onSearchbarSubmit = useCallback(
-    (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      setSubmitted(text);
-      if (!submitted) {
-        return;
-      } else {
-        fetchData(submitted);
-        setText("");
-      }
-    },
-    [text, submitted, fetchData]
-  );
-  console.log(keyword, text, submitted);
-  console.log(submitted);
 
-  async function fetchData(submitted: string) {
-    const breweriesData = await handleKeywords(submitted);
-    console.log(breweriesData);
-    if (!(breweriesData.length > 0)) {
-      console.log("no data");
-      return;
-    } else {
-      console.log("your results");
-    }
-  }
-
-  async function handleKeywords(submitted: string) {
-    if (keyword === "country") {
-      return searchByCountry(submitted);
-    } else if (keyword === "any") {
-      return searchByAny(submitted);
-    } else return searchByNameOrCity(submitted, keyword);
-  }
-
-  async function searchByCountry(submitted: string) {
-    const capitalizedQuery = capitalizeFirstLetter(submitted);
-    const responseCountry = await fetch(
-      `${baseEndpoint}/search?query={${capitalizedQuery}}&per_page=200`
-    );
-    const unfilteredData = await responseCountry.json();
-    const dataCountry = [];
-    for (const breweryData of unfilteredData) {
-      if (breweryData.country === capitalizedQuery) {
-        dataCountry.push(breweryData);
-      }
-    }
-    return dataCountry;
-  }
-
-  async function searchByAny(submitted: string) {
-    const responseAny = await fetch(
-      `${baseEndpoint}/search?query={${submitted}}&per_page=200`
-    );
-    const dataAny = await responseAny.json();
-    return dataAny;
-  }
-
-  async function searchByNameOrCity(submitted: String, keyword: string) {
-    const responseNameOrCity = await fetch(
-      `${baseEndpoint}?by_${keyword}=${submitted}&per_page=200`
-    );
-    const dataNameOrCity = await responseNameOrCity.json();
-    return dataNameOrCity;
-  }
-
-  function capitalizeFirstLetter(submitted: string) {
+  const capitalizeFirstLetter = useCallback((submitted: string) => {
     const words = submitted.split(" ");
     return words
       .map((word) => {
         return word[0].toUpperCase() + word.substring(1).toLowerCase();
       })
       .join(" ");
-  }
+  }, []);
+
+  const searchByCountry = useCallback(
+    async (submitted: string) => {
+      const capitalizedQuery = capitalizeFirstLetter(submitted);
+      const responseCountry = await fetch(
+        `${baseEndpoint}/search?query={${capitalizedQuery}}&per_page=200`
+      );
+      const unfilteredData = await responseCountry.json();
+      const dataCountry = [];
+      for (const breweryData of unfilteredData) {
+        if (breweryData.country === capitalizedQuery) {
+          dataCountry.push(breweryData);
+        }
+      }
+      return dataCountry;
+    },
+    [capitalizeFirstLetter]
+  );
+
+  const searchByAny = useCallback(async (submitted: string) => {
+    console.log(submitted);
+    const responseAny = await fetch(
+      `${baseEndpoint}/search?query={${submitted}}&per_page=200`
+    );
+    const dataAny = await responseAny.json();
+    return dataAny;
+  }, []);
+
+  const searchByNameOrCity = useCallback(
+    async (submitted: String, keyword: string) => {
+      const responseNameOrCity = await fetch(
+        `${baseEndpoint}?by_${keyword}=${submitted}&per_page=200`
+      );
+      const dataNameOrCity = await responseNameOrCity.json();
+      return dataNameOrCity;
+    },
+    []
+  );
+
+  const handleKeywords = useCallback(
+    async (submitted: string) => {
+      if (keyword === "country") {
+        return searchByCountry(submitted);
+      } else if (keyword === "any") {
+        return searchByAny(submitted);
+      } else return searchByNameOrCity(submitted, keyword);
+    },
+    [keyword, searchByAny, searchByCountry, searchByNameOrCity]
+  );
+  
+  const fetchData = useCallback(
+    async (submitted: string) => {
+      const breweriesData = await handleKeywords(submitted);
+      console.log(breweriesData);
+      if (!(breweriesData.length > 0)) {
+        console.log("no data");
+        return;
+      } else {
+        console.log("your results");
+      }
+    },
+    [handleKeywords]
+  );
+
+  const onSearchbarSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setSubmitted(text);
+      setText("");
+      if (!submitted) {
+        return;
+      } else {
+        fetchData(submitted);
+      }
+    },
+    [text, submitted, fetchData]
+  );
+  console.log(keyword, text, submitted);
+  console.log(submitted);
 
   return (
     <>
