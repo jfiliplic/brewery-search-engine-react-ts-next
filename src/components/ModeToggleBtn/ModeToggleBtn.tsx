a9a79a1454a8093d38ae2c197d5129102b8f2495
jfiliplic <janezfiliplic@gmail.com>
2023-08-09 00:54:28 +0200

    dark mode toggle and prefered color scheme

--- src/components/ModeToggleBtn/ModeToggleBtn.tsx
+++ src/components/ModeToggleBtn/ModeToggleBtn.tsx
@@ -1,10 +1,55 @@
 import styles from "./ModeToggleBtn.module.scss";
+import { useCallback, useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export const ModeToggleBtn = () => {
+  // const root = document.querySelector(":root");
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDark]);
+
+  const handleToggleBtn = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setIsDark(e.target.checked);
+    },
+    []
+  );
+
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined,
+    (isSystemDark) => {
+      setIsDark(isSystemDark);
+      setIsDark(true); //??
+    }
+  );
+
+  // useEffect(() => {
+  //   if (isDark) {
+  //     root.setAttribute("dark", "");
+  //   } else {
+  //     root.removeAttribute("dark");
+  //   }
+  // }, [isDark, root]);
+
   return (
     <>
       <label htmlFor="toggle" className={styles.toggle}>
-        <input type="checkbox" id="toggle" className={styles.toggleInput} />
+        <input
+          type="checkbox"
+          id="toggle"
+          className={styles.toggleInput}
+          checked={!isDark} //??
+          onChange={handleToggleBtn}
+        />
         <span className={styles.toggleButton}></span>
       </label>
     </>
