3040b4e3a56733505262bd3a6d46e57bd4ab5da8
jfiliplic <janezfiliplic@gmail.com>
2023-08-20 14:25:32 +0200

    creating html for result cards with slice&map given resultsPerPage value

--- src/components/ResultCards/ResultCards.tsx
+++ src/components/ResultCards/ResultCards.tsx
@@ -15,6 +15,7 @@ export function ResultCards({
   const numberOfSteps = Math.floor(totalResults / resultsPerPage);
   const resultsBehind = resultPageNumber * resultsPerPage;
   const resultsCurrentAhead = totalResults - resultsBehind;
+
   if (totalResults - resultsBehind < resultsPerPage) {
     resultsPerPage = totalResults - resultsBehind;
   }
@@ -32,72 +33,62 @@ export function ResultCards({
     );
   }
 
-  // return (
-  //   <tbody>
-  //     {(() => {
-  //       const rows = [];
-  //       for (let i = 0; i < objectRows.length; i++) {
-  //         rows.push(<ObjectRow key={i} data={objectRows[i]} />);
-  //       }
-  //       return rows;
-  //     })()}
-  //   </tbody>
-  // );
   return (
     <div className={styles.resultCards}>
       {(() => {
-        let htmlResultCards = [];
-        for ( let i = 0 + resultsBehind;
-      resultsCurrentAhead > resultsPerPage
-      ? i < resultsPerPage + resultsBehind
-      : i < totalResults;
-      i++) {
-      const htmlResultCard = [queryResults[i]].map((id, name, city, country) => (
-        <Link
-          key={id}
-          className={styles.singleResultLink}
-          href={`/brewery/${id}`}
-        >
-          <div className={styles.singleCard}>
-            <h2>
-              {name}
-              <span>/</span>
-            </h2>
-            <h2>
-              {city}
-              <span>/</span>
-            </h2>
-            <h2>{country}</h2>
-          </div>
-        </Link>
-      ));
-      htmlResultCards.push(htmlResultCard);
-    }
+        const htmlResultCard = queryResults
+          .slice(
+            resultsBehind,
+            resultsCurrentAhead > resultsPerPage
+              ? resultsPerPage + resultsBehind
+              : totalResults
+          )
+          .map(({ id, name, city, country }) => (
+            <Link
+              key={id}
+              className={styles.singleResultLink}
+              href={`/brewery/${id}`}
+            >
+              <div className={styles.singleCard}>
+                <h2>
+                  {name}
+                  <span>/</span>
+                </h2>
+                <h2>
+                  {city}
+                  <span>/</span>
+                </h2>
+                <h2>{country}</h2>
+              </div>
+            </Link>
+          ));
+        return htmlResultCard;
+      })()}
     </div>
   );
-      
-
-  // return (
-  //   <div className={styles.resultCards}>
-  //     {queryResults.map((result) => (
-  //       <Link
-  //         key={result.id}
-  //         className={styles.singleResultLink}
-  //         href={`/brewery/${result.id}`}
-  //       >
-  //         <div className={styles.singleCard}>
-  //           <h2>
-  //             {result.name}
-  //             <span>/</span>
-  //           </h2>
-  //           <h2>
-  //             {result.city}
-  //             <span>/</span>
-  //           </h2>
-  //           <h2>{result.country}</h2>
-  //         </div>
-  //       </Link>
-  //     ))}
-  //   </div>
-  // );
 }
+
+//   return (
+//     <div className={styles.resultCards}>
+//       {queryResults.map((result) => (
+//         <Link
+//           key={result.id}
+//           className={styles.singleResultLink}
+//           href={`/brewery/${result.id}`}
+//         >
+//           <div className={styles.singleCard}>
+//             <h2>
+//               {result.name}
+//               <span>/</span>
+//             </h2>
+//             <h2>
+//               {result.city}
+//               <span>/</span>
+//             </h2>
+//             <h2>{result.country}</h2>
+//           </div>
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// }
