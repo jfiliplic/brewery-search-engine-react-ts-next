28a7b2ee1ad201e81c4ed57664665c553c7b2a7d
jfiliplic <janezfiliplic@gmail.com>
2023-09-15 14:46:34 +0200

    added missing types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import Layout from "@/components/Layout/Layout";
+import type { AppProps } from "next/app";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />;
