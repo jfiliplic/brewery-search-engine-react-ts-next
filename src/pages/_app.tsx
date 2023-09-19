bd5311a745988e4d96db399101902e1379c112c2
jfiliplic <janezfiliplic@gmail.com>
2023-09-19 11:29:20 +0200

    refactored kavoon font using next/font/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import Layout from "@/components/Layout/Layout";
 import type { AppProps } from "next/app";
+import { Kavoon } from "next/font/google";
 import "../styles/globals.scss";
+
+const kavoon = Kavoon({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-content",
+  fallback: ["cursive"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <div className={kavoon.variable}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </div>
   );
 }
 
